5413f4b53dc732766643b37a834078123b08942b
SBoesten <58263226+SBoesten@users.noreply.github.com>
2019-11-28 13:04:09 +0100

    Add files via upload

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$fileLocation = Join-Path $toolsDir 'avira_en_free0___maj.exe' 
+
+$packagename = 'avira'
+
+$packageArgs = @{
+  packageName   = $packagename
+  fileType      = 'EXE' 
+  file         = $fileLocation	
+  softwareName  = 'avira*'
+  validExitCodes= @(0) 		
+  silentArgs    = "/S" 				
+ 
+
+}
+
+Install-ChocolateyInstallPackage @packageArgs
